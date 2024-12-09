86dc65b0f659e158418de8e812c3a334177b3d0d
vienne <vienne.francois@outlook.fr>
2024-12-09 12:12:14 +0100

    ajout de page film et page series

---
+++ pages/film.php
@@ -0,0 +1,68 @@
+<?php
+include("header.php");
+?>
+
+<div class="movie">
+
+        <p>Film comedie</p>
+        <ul class="align_movie">
+            <li class="poster">
+                <a href="#"> <img src="https://fr.web.img2.acsta.net/pictures/18/07/02/17/25/3643090.jpg" alt="mov" ></a>
+            </li>
+            <li class="poster">
+                <a href="#"> <img src="https://fr.web.img2.acsta.net/pictures/18/07/02/17/25/3643090.jpg" alt="mov" ></a>
+            </li>
+            <li class="poster">
+                <a href="#"> <img src="https://fr.web.img2.acsta.net/pictures/18/07/02/17/25/3643090.jpg" alt="mov" ></a>
+            </li>
+            <li class="poster">
+                <a href="#"> <img src="https://fr.web.img2.acsta.net/pictures/18/07/02/17/25/3643090.jpg" alt="mov" ></a>
+            </li>
+            <li class="poster">
+                <a href="#"> <img src="https://fr.web.img2.acsta.net/pictures/18/07/02/17/25/3643090.jpg" alt="mov" ></a>
+            </li>
+        </ul>
+
+
+        <p>
+            Film d'action
+        </p>
+        <ul class="align_movie">
+            <li class="poster">
+                <a href="#"> <img src="https://fr.web.img2.acsta.net/pictures/18/07/02/17/25/3643090.jpg" alt="mov" ></a>
+            </li>
+            <li class="poster">
+                <a href="#"> <img src="https://fr.web.img2.acsta.net/pictures/18/07/02/17/25/3643090.jpg" alt="mov" ></a>
+            </li>
+            <li class="poster">
+                <a href="#"> <img src="https://fr.web.img2.acsta.net/pictures/18/07/02/17/25/3643090.jpg" alt="mov" ></a>
+            </li>
+            <li class="poster">
+                <a href="#"> <img src="https://fr.web.img2.acsta.net/pictures/18/07/02/17/25/3643090.jpg" alt="mov" ></a>
+            </li>
+            <li class="poster">
+                <a href="#"> <img src="https://fr.web.img2.acsta.net/pictures/18/07/02/17/25/3643090.jpg" alt="mov" ></a>
+            </li>
+        </ul>
+
+
+        <p>Film fantasy</p>
+        <ul class="align_movie">
+            <li class="poster">
+                <a href="#"> <img src="https://fr.web.img2.acsta.net/pictures/18/07/02/17/25/3643090.jpg" alt="mov" ></a>
+            </li>
+            <li class="poster">
+                <a href="#"> <img src="https://fr.web.img2.acsta.net/pictures/18/07/02/17/25/3643090.jpg" alt="mov" ></a>
+            </li>
+            <li class="poster">
+                <a href="#"> <img src="https://fr.web.img2.acsta.net/pictures/18/07/02/17/25/3643090.jpg" alt="mov" ></a>
+            </li>
+            <li class="poster">
+                <a href="#"> <img src="https://fr.web.img2.acsta.net/pictures/18/07/02/17/25/3643090.jpg" alt="mov" ></a>
+            </li>
+            <li class="poster">
+                <a href="#"> <img src="https://fr.web.img2.acsta.net/pictures/18/07/02/17/25/3643090.jpg" alt="mov" ></a>
+            </li>
+        </ul>
+
+</div>
