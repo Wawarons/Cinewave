f8dcc14beefcd69d0ba2147bd72bcdc989eebf1e
philippe.distinguin <philippe.distinguin62@outlook.com>
2025-01-23 11:03:25 +0100

    modif page film et series CSS

--- serie.php
+++ serie.php
@@ -1,29 +1,32 @@
 <?php
 include("includes/header.php");
 include("includes/queries.php");
+
+// Récupérer les données des films
 $dataSeries = getSeries();
+
+// Diviser les films en sections de 5
+$chunks = array_chunk($dataSeries, 5);
 ?>
-
-<div class="top">
-    <h2>SÉRIES</h2>
-    <p>Séries comedie</p>
-    <div class="top_contenu">
-        <?php
-        foreach ($dataSeries as $index => $serie) {
-
-            echo "
-                 <div class='poster'>
-                    <a href='about.php?type=serie&title=" . htmlspecialchars($serie['title'], ENT_QUOTES, 'UTF-8') . "'>
-                        <img src='" . htmlspecialchars($serie['image'], ENT_QUOTES, 'UTF-8') . "' alt='affiche de film'>
-                    </a>
-                 </div>
-                ";
+<div class="align_movie">
+    <?php
+    // Parcourir les sections
+    foreach ($chunks as $sectionIndex => $series) {
+        echo "<div class='movie-row'>"; // Chaque section devient une ligne
+        foreach ($dataSeries as $series) {
+            echo "<div class='containt_affiche'>
+                     <div class='poster'>
+                        <a href='about.php?type=film&title=" . htmlspecialchars($series['title'], ENT_QUOTES, 'UTF-8') . "'>
+                            <img src='" . htmlspecialchars($series['image'], ENT_QUOTES, 'UTF-8') . "' alt='affiche de series' class='poster-image'>
+                        </a>
+                        <div class='rate-container'>
+                            <span class='rating-text'>4.2/5</span>
+                            <img src='assets/images/hearth.svg' alt='heart icon' width='10px' height='10px'>
+                        </div>
+                     </div>
+                 </div>";
         }
-        ?>
-    </div>
-</div>
-
-
-
-
+        echo "</div>"; // Fin de la ligne
+    }
+    ?>
 </div>
