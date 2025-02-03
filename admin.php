259236495fe0303851aef8dd5543635d86f75757
vienne <vienne.francois@outlook.fr>
2025-02-03 08:57:32 +0100

    modif css page admin

--- admin.php
+++ admin.php
@@ -3,10 +3,14 @@ include('./includes/header.php')
 ?>
 <div class="admin_menu">
 <p class="menu_text">Menu</p>
-    <button class="button_menu">Lorem ipsum dolor sit amet.</button>
-    <button class="button_menu">Lorem ipsum dolor sit amet.</button>
-    <button class="button_menu">Lorem ipsum dolor sit amet.</button>
-    <button class="button_menu">Lorem ipsum dolor sit amet.</button>
-    <button class="button_menu">Lorem ipsum dolor sit amet.</button>
-    <button class="button_menu">Lorem ipsum dolor sit amet.</button>
+    <button class="button_menu">Tableau de Bord </button>
+    <button class="button_menu">Gestion des Utilisateurs</button>
+    <button class="button_menu">Gestion du Contenu</button>
+    <button class="button_menu">Gestion des Médias</button>
+    <button class="button_menu">Configuration du Site</button>
+    <button class="button_menu">Statistiques et Rapports</button>
+    <button class="button_menu">Gestion des Commentaires et Avis</button>
+    <button class="button_menu">Sécurité et Permissions</button>
+    <button class="button_menu">Extensions et Modules</button>
+
 </div>
