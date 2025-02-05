5e798621054acbebf0ee815cb23fdc578f56b68c
Distinguin Philippe <phil62750@hotmail.fr>
2025-02-05 16:09:46 +0100

    Création page contact et stylisé bouton page inscription

--- inscription.php
+++ inscription.php
@@ -9,25 +9,27 @@ if(isset($_SESSION["errors_inscription"])){
     echo "<div class='form_error'><p>".$_SESSION["errors_inscription"][0]."</p></div>";
 }
 ?>
+
 <form action="utilities/inscriptionUtilisateur.php" method="POST" id="inscription">
     <div class="form_input">
-        <label for="email"> Mail:</label>
-        <input id="email" type="email" name="email" class="text" required/>
+        <label for="email">Mail :</label>
+        <input id="email" type="email" name="email" class="text" required />
     </div>
     <br>
     <div class="form_input">
-        <label for="username">Pseudo:</label>
-        <input id="username" type="text" name="username" class="text" required>
+        <label for="username">Pseudo :</label>
+        <input id="username" type="text" name="username" class="text" required />
     </div>
     <br>
     <div class="form_input">
-        <label for="password">Mot de passe:</label>
-        <input id="password" type="password" name="password" class="text" required>
+        <label for="password">Mot de passe :</label>
+        <input id="password" type="password" name="password" class="text" required />
     </div>
     <br>
     <div class="form_input">
-        <label for="confirm_password">Confirmation de mot de passe:</label>
-        <input id="confirm_password" name="confirm_password" type="password" class="text" required>
+        <label for="confirm_password">Confirmation de mot de passe :</label>
+        <input id="confirm_password" name="confirm_password" type="password" class="text" required />
     </div>
-    <input type="submit" class="submit">
+    <br>
+    <input type="submit" class="submit" value="Envoyer">
 </form>
